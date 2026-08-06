cbd2ec66c40cc21c889f3fbb129c301e9227d69a
Dashrath Singh <dashrath@example.com>
2026-08-06 16:09:02 +0530

    Force secure cookie policies and credentials include

--- config.py
+++ config.py
@@ -12,5 +12,7 @@ class Config:
         "sqlite:///" + os.path.join(INSTANCE_DIR, "sweets.db").replace("\\", "/"),
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SECURE = True
     RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID", "rzp_test_TM6u7P2ViMrI2I")
     RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET", "UW6HuVcZYZgLmVLYwNE51q3P")
